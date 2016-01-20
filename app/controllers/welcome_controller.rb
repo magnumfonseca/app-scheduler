93249a02a2c6499fd9e364f277ba03e59dee20b1
magnumfonseca <magnum.td.fonseca@gmail.com>
2016-01-19 22:11:26 -0200

    generate event model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   before_filter :authenticate_user!
+
   def index
+    current_week = Date.today.cweek
+    @events = Event.find_by_week(current_week)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+  def table_head
+    content_tag :thead, class: "gainsboro" do
+      content_tag :tr do
+        concat hours_label
+        days_of_week.each do |day|
+          concat days_of_week_label(day)
+        end
+      end.html_safe
+    end.html_safe
+  end
+
+
+  def generate_hour_range
+    hours = []
+    hour_step = (1.to_f/24)
+    date_init = DateTime.new.change({ hour: 6 })
+    date_limit = DateTime.new.change({ hour: 23 })
+    date_init.step(date_limit, hour_step).each do |date|
+      hours << "#{sprintf '%02d', date.hour}:00"
+    end
+    hours
+  end
+
+  def days_of_week
+    ['seg','ter','qua', 'qui', 'sex']
+  end
+
+  private
+  def days_of_week_label(label)
+    content_tag :th, label
+  end
+
+  def hours_label
+    content_tag :th, "Horário"
+  end
 end
